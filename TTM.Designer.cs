d33c346c5aacf05a3f7621ad124bf390b084cd94
xjiujiu <xjiujiu@gmail.com>
2014-03-28 22:15:47 +0800

    同步工程
    1. 同步工程窗体名

--- TTM.Designer.cs
+++ TTM.Designer.cs
@@ -3,14 +3,14 @@
     partial class TTM
     {
         /// <summary>
-        /// 必需的设计器变量。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清理所有正在使用的资源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing) {
             if (disposing && (components != null)) {
                 components.Dispose();
@@ -18,11 +18,11 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要
-        /// 使用代码编辑器修改此方法的内容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
             this.SuspendLayout();
@@ -31,7 +31,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 369);
+            this.ClientSize = new System.Drawing.Size(290, 261);
             this.Name = "TTM";
             this.Text = "TTM";
             this.ResumeLayout(false);
@@ -41,4 +41,3 @@
         #endregion
     }
 }
-
